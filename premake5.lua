d0127a1c09b55d718570d76dd0f261410dc77cc9
Spagathore <nobodyknowme1437@gmail.com>
2025-06-22 05:34:03 +0530

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "GameEngine_"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine_"
+	location "GameEngine_"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS",
+			"GE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine_/vendor/spdlog/include",
+		"GameEngine_/src"
+	}
+
+	links
+	{
+		"GameEngine_"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
